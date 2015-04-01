b95cb4dafc855d33dede0cea79d9e81680d43104
AlexKnauth <alexander@knauth.org>
2015-04-01 17:23:33 -0400

    add info.rkt

---
+++ info.rkt
@@ -0,0 +1,15 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("base"
+               "generic-bind"
+               "rackjure"
+               "kw-utils"
+               "match-string"
+               "git://github.com/AlexKnauth/my-plai"
+               ))
+
+(define build-deps '("rackunit-lib"
+                     ))
+
